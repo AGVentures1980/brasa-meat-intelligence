8efd7311281ce16c2633ae2b2f323d72fda02c46
AGVentures1980 <xande1980@gmail.com>
2026-02-05 09:37:28 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,9 +1,20 @@
+from sqlalchemy import Column, Integer, String, Float, Date
+from app.database import Base
+
 class Order(Base):
     __tablename__ = "orders"
 
     id = Column(Integer, primary_key=True, index=True)
-    store_id = Column(Integer)
-    order_id = Column(String)
-    item_name = Column(String)
+    store_id = Column(Integer, index=True)
+    order_id = Column(String, index=True)
+    item_name = Column(String, index=True)
     qty = Column(Float)
-    order_date = Column(DateTime)
+    order_date = Column(Date)
+
+class Recipe(Base):
+    __tablename__ = "recipes"
+
+    id = Column(Integer, primary_key=True, index=True)
+    item_name = Column(String, unique=True, index=True)
+    cut = Column(String)
+    qty_lb = Column(Float)  # consumo por unidade
