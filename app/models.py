24e296ddfae092fb822617327b3fe999b6525755
AGVentures1980 <xande1980@gmail.com>
2026-02-05 09:16:38 -0500

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,14 +1,9 @@
-from sqlalchemy import Column, Integer, String, Numeric, Date, ForeignKey
-from app.database import Base
-from datetime import date
-
-class MeatUsage(Base):
-    __tablename__ = "meat_usage"
+class Order(Base):
+    __tablename__ = "orders"
 
     id = Column(Integer, primary_key=True, index=True)
-    store_id = Column(Integer, nullable=False)
-    cut = Column(String(100), nullable=False)
-    received_qty = Column(Numeric(10,2), nullable=False)
-    used_qty = Column(Numeric(10,2), nullable=False)
-    waste_qty = Column(Numeric(10,2), nullable=False)
-    record_date = Column(Date, default=date.today)
+    store_id = Column(Integer)
+    order_id = Column(String)
+    item_name = Column(String)
+    qty = Column(Float)
+    order_date = Column(DateTime)
